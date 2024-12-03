07f69ed31b10c47950a9dac09d38b53c43f8ba85
turly221 <feifan@scntist.com>
2024-12-03 21:17:09 +0000

    commit patch 22561554

--- kioslave/http/http.cpp
+++ kioslave/http/http.cpp
@@ -1584,7 +1584,7 @@ QString HTTPProtocol::davError( int code /* = -1 */, const QString &_url )
 
   QString url = _url;
   if ( !url.isNull() )
-    url = m_request.url.url();
+    url = m_request.url.prettyUrl();
 
   QString action, errorString;
   int errorCode = ERR_SLAVE_DEFINED;
@@ -3642,7 +3642,7 @@ endParsing:
             KUrl u(m_request.url, locationStr);
             if(!u.isValid())
             {
-                error(ERR_MALFORMED_URL, u.url());
+                error(ERR_MALFORMED_URL, u.prettyUrl());
                 return false;
             }
 
